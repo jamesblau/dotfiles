b947d7481ac9a495271970f6aa700aee4d3ca0da
James Blau <james@tresata.com>
2016-08-04 16:21:32 -0400

    Mess with trackpoint speed in conf; update pactl volume controls in i3 conf; change screen type for new laptop in i3; add nm-applet and i3lock service to startup in i3; format some screenlayout files; fix that annoying thing where vim would indent scala files wrong after typing '>'; fix font size for firefox in vimperatorrc

--- screenlayout/default.conf.sh
+++ screenlayout/default.conf.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 xrandr \
-        --output DP3 --off \
-        --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal \
-        --output DP1 --off \
-        --output DP2 --off \
-        --output HDMI3 --off \
-        --output HDMI2 --off \
-        --output HDMI1 --off \
-        --output VGA1 --off
+  --output VIRTUAL1 --off \
+  --output DP3 --off \
+  --output eDP1 --primary --mode 2560x1440 --pos 0x0 --rotate normal \
+  --output DP1 --off \
+  --output HDMI3 --off \
+  --output HDMI2 --off \
+  --output HDMI1 --off \
+  --output DP2 --off
