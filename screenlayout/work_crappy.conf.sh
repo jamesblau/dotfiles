c4260517108710cc6261a6f1b1b912504ae21555
James Blau <james@tresata.com>
2017-05-12 14:32:53 -0400

    In bashrc: set nvim runtime to inside src/opensource/neovim, add rmempty and treeu commands; in conf: add 'crappy' layouts, tweek stick speed; in i3 config: VIMRUNTIME must always be set for special terminals, add more emoticons and open-in-chrome command; vimperator/pentadactyl: munge google etc. into google_search etc., add some <Nop>'d out commands and some mappings to wat things should do anyway; in vimrc, remove empty LoadCsv function; in vimrc.bundles: highlight whitespace; move to new CtrlP; custom nerdcommenter delimiter and mapping to match scaladoc comments; fix SetScalaOptions autocommand

---
+++ screenlayout/work_crappy.conf.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output DP3 --off --output eDP1 --off --output DP1 --off --output HDMI3 --off --output HDMI2 --primary --mode 1920x1200 --pos 0x0 --rotate normal --output HDMI1 --off --output DP2 --off
