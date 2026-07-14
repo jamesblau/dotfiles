48a2dce1929b89051336c4c6a1eea2e999886ecb
James Blau <jamesblau@gmail.com>
2026-07-13 22:30:59 -0400

    i3status-custom.sh: add smash-mode marker.
    screen-awake: update, still not perfect.
    
    sleep-poweroff-etc: rename shutdown to poweroff (including in script name).
    
    smash-mode: update, still not perfect.
    
    Rename togeth to toggle-ethernet.
    
    toggle-mic: for some reason needed to be rewritten to tell the mic when it's software muted.
    
    i3/config: remove bin/ path segments as ~/bin is on PATH, and add smash-mode toggles, sleep script renamed.
    
    zaphod_vert.conf.sh: try 60 Hz

--- bin/i3status-custom.sh
+++ bin/i3status-custom.sh
@@ -18,6 +18,7 @@
 #   status_command measure-net-speed-i3status.bash
 # }
 
+smash_marker="${XDG_RUNTIME_DIR:-/tmp}/smash-mode-$UID/enabled"
 i3status | (read line && echo $line && read line && echo $line && while :
 do
   read line
@@ -37,13 +38,34 @@ do
     rm ~/.myip
   fi
 
-  # Audio stuff
-  volume=$(<<< "${line}" sed 's/^.*\({[^{}]*}\)]$/\1/')
-  level_status=$(amixer sget Capture | tail -n 1 | cut -d '[' -f 2,4 | sed 's/[][]//g')
-  color=$(<<< ${level_status} grep -q off && echo ',"color":"#dfaf8f"')
-  mic="{ \"full_text\": \"🎤 ${level_status/ */}\" ${color}}"
+    # Audio stuff
+    volume=$(
+        sed 's/^.*\({[^{}]*}\)]$/\1/' <<<"$line"
+    )
 
-  echo "${line/$volume/$mic, $volume}" || exit 1
+    level_status=$(
+        amixer sget Capture |
+            tail -n 1 |
+            cut -d '[' -f 2,4 |
+            sed 's/[][]//g'
+    )
+
+    color=$(
+        grep -q off <<<"$level_status" &&
+            echo ',"color":"#dfaf8f"'
+    )
+
+    mic="{ \"full_text\": \" ${level_status/ */}\" ${color}}"
+
+    # Smash-mode indicator
+    extra_blocks="$mic"
+
+    if [[ -e "$smash_marker" ]]; then
+        smash='{ "full_text": " 💥", "color": "#c87878" }'
+        extra_blocks="$smash, $extra_blocks"
+    fi
+
+    echo "${line/$volume/$extra_blocks, $volume}" || exit 1
   # TODO: Why doesn't this less weird version work? Some special character?
   # before_volume=$(echo "${line}" | sed 's/^,\[//' | sed 's/^\(.*\),{[^{}]*}]$/\1/')
   # echo ",[${before_volume},${mic},${volume}]" || exit 1
